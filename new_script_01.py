ad0a300dbd866dbb0fe6d8325fec3fd10c73eeb8
davesh9123-alt <davesh9123@gmail.com>
2025-11-27 14:25:02 -0600

    update python script

--- new_script_01.py
+++ new_script_01.py
@@ -1,1 +1,11 @@
+import os
 print('Hola, estamos probando VS Code con GitHub')
+
+
+print(f'My working directory is: {os.getcwd()}')
+
+x = 10
+if x % 2 == 0:
+    print(f"{x} is an even number")
+else:
+    print(f"{x} is an odd number")
